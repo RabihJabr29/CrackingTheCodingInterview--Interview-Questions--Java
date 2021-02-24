9c2b201c0c1dc58e0f31a77819a0a74fcb02f223
RabihJabr29 <rabihjabrz2@gmail.com>
2021-02-24 18:54:44 +0200

    UndirectedGraph - V1

--- Graphs/UndirectedGraph.java
+++ Graphs/UndirectedGraph.java
@@ -25,34 +25,38 @@ public class UndirectedGraph {
 	public void printRecursiveDFS() {
 		System.out.println("Graph DFS: ");
 		int n = countVertices();
-		int[] color = new int[n];
+		int[] visited = new int[n];
 
+		// you can change the source, however, usually it starts 
+		// from the lowest numbered vertex
+		int src = 0;
+		
 		// you can use this assuming that the graph is a connected graph.
-		printRecursiveDFS(0, color);
+		printRecursiveDFS(src, visited);
 
 		// if the graph is not a connected graph you can run the below code.
 		// just remove the //s
 
 //		int count = 0;
 //		for (int i = 0; i < n; i++) {
-//			if (color[i] == 0) {
+//			if (visited [i] == 0) {
 //				System.out.print("Subgraph #" + count + ": ");
 //				count++;
-//				printRecursiveDFS(i, color);
+//				printRecursiveDFS(i, visited );
 //				System.out.println();
 //			}
 //		}
 
 	}
 
-	private void printRecursiveDFS(int u, int[] color) {
-		if (color[u] != 1) {
-			System.out.print(u + " ");
-			color[u] = 1;
+	private void printRecursiveDFS(int src, int[] visited ) {
+		if (visited [src] != 1) {
+			System.out.print(src + " ");
+			visited [src] = 1;
 		}
-		for (int v : this.adjacencyList.get(u)) {
-			if (color[v] == 0) {
-				printRecursiveDFS(v, color);
+		for (int vertex : this.adjacencyList.get(src)) {
+			if (visited [vertex] == 0) {
+				printRecursiveDFS(vertex, visited );
 			}
 		}
 	}
@@ -60,23 +64,23 @@ public class UndirectedGraph {
 	public void printBFS() {
 		System.out.println("Graph BFS: ");
 		int n = countVertices();
-		int[] color = new int[n];
+		int[] visited = new int[n];
 
 		Queue<Integer> q = new LinkedList<>();
 		int count = 0;
 		for (int i = 0; i < n; i++) {
-			if (color[i] == 0) {
+			if (visited[i] == 0) {
 				System.out.print("Subgraph #" + count + ": ");
 				q.add(i);
-				color[i] = 1;
+				visited[i] = 1;
 				while (!q.isEmpty()) {
-					int curV = q.poll();
-					color[curV] = 2;
-					System.out.print(curV + " ");
-					for (int neighbor : this.adjacencyList.get(curV)) {
-						if (color[neighbor] == 0) {
+					int currenyVertex = q.poll();
+					visited[currenyVertex] = 2;
+					System.out.print(currenyVertex + " ");
+					for (int neighbor : this.adjacencyList.get(currenyVertex)) {
+						if (visited[neighbor] == 0) {
 							q.add(neighbor);
-							color[neighbor] = 1;
+							visited[neighbor] = 1;
 						}
 					}
 				}
